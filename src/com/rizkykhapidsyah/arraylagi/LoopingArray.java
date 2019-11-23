c83596242fb8959648ed2145d28d0c4492fe3dd8
Rizky Khapidsyah <45317201+RizkyKhapidsyah@users.noreply.github.com>
2019-11-24 03:16:45 +0700

    Update LoopingArray.java

--- src/com/rizkykhapidsyah/arraylagi/LoopingArray.java
+++ src/com/rizkykhapidsyah/arraylagi/LoopingArray.java
@@ -4,7 +4,7 @@ public class LoopingArray {
     public static void main(String[] args) {
         int[] X = new int[1000];
 
-        // Inisiasi dan menampilkan X dari elemen ke 1 sampai 1000000
+        // Inisiasi dan menampilkan X dari elemen ke 1 sampai 1000
         for (int i = 0; i < X.length; i++) {
             X[i] = i + 1;
             System.out.println(X[i]);
